e9e5a89bc64171ae4f7423b0f0e058117d16154c
MZakaraia <dr.mzakaraia@gmail.com>
2025-04-03 19:43:34 +0200

    Initial Genetic Algorithm 1

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,29 +1,30 @@
 import numpy as np
 
-
 class GeneticAlgorithm:
     def __init__(self, objective_func, population_size=50, dimensions=3, 
                  mutation_rate=0.1, crossover_rate=0.9,
-                 bounds=(-5.12, 5.12), max_generations=100):
+                 bounds=(-5.12, 5.12), max_generations=100, elite_size=1):
         self.pop_size = population_size
         self.dims = dimensions
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
         self.bounds = bounds
         self.max_gens = max_generations
+        self.elite_size = elite_size
         self.fn = objective_func
         
     def initialize_population(self):
         return np.random.uniform(self.bounds[0], self.bounds[1], 
-                               (self.pop_size, self.dims))
+                                 (self.pop_size, self.dims))
     
     def evaluate_fitness(self, population):
-        return np.array([1/(1 + self.fn(ind)) for ind in population])
+        return np.array([1 / (1 + self.fn(ind)) for ind in population])
     
-    def select_parents(self, population, fitness):
-        indices = np.random.choice(np.arange(self.pop_size), size=2, 
-                                 p=fitness/fitness.sum())
-        return population[indices[0]], population[indices[1]]
+    def tournament_selection(self, population, fitness, k=3):
+        """Tournament Selection: Picks the best out of k randomly chosen individuals."""
+        selected = np.random.choice(np.arange(self.pop_size), size=k, replace=False)
+        best_idx = selected[np.argmax(fitness[selected])]
+        return population[best_idx]
     
     def crossover(self, parent1, parent2):
         if np.random.rand() < self.crossover_rate:
@@ -33,10 +34,12 @@ class GeneticAlgorithm:
             return child1, child2
         return parent1.copy(), parent2.copy()
     
-    def mutate(self, individual):
+    def mutate(self, individual, generation):
+        """Adaptive mutation: reduces mutation rate over generations."""
+        adaptive_rate = self.mutation_rate * (1 - generation / self.max_gens)
         for i in range(self.dims):
-            if np.random.rand() < self.mutation_rate:
-                individual[i] += np.random.normal(0, 0.5)
+            if np.random.rand() < adaptive_rate:
+                individual[i] += np.random.normal(0, 0.3 * (1 - generation / self.max_gens))
                 individual[i] = np.clip(individual[i], *self.bounds)
         return individual
     
@@ -46,20 +49,22 @@ class GeneticAlgorithm:
         
         for gen in range(self.max_gens):
             fitness = self.evaluate_fitness(population)
-            best_idx = np.argmin([self.fn(ind) for ind in population])
-            best_fitness.append(self.fn(population[best_idx]))
+            sorted_indices = np.argsort(-fitness)  # Sort in descending order
+            best_individuals = population[sorted_indices[:self.elite_size]]  # Preserve elites
             
-            new_population = []
-            for _ in range(self.pop_size//2):
-                p1, p2 = self.select_parents(population, fitness)
+            best_fitness.append(self.fn(population[sorted_indices[0]]))
+            
+            new_population = list(best_individuals)  # Start with elites
+            
+            while len(new_population) < self.pop_size:
+                p1 = self.tournament_selection(population, fitness)
+                p2 = self.tournament_selection(population, fitness)
                 c1, c2 = self.crossover(p1, p2)
-                c1 = self.mutate(c1)
-                c2 = self.mutate(c2)
+                c1 = self.mutate(c1, gen)
+                c2 = self.mutate(c2, gen)
                 new_population.extend([c1, c2])
-                
-            population = np.array(new_population)
+            
+            population = np.array(new_population[:self.pop_size])  # Ensure correct population size
         
         self.bestFitness = best_fitness[-1]
-            
-        return population[best_idx], self.bestFitness , best_fitness
-
+        return population[sorted_indices[0]], self.bestFitness, best_fitness
